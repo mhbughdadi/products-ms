0dc33f403da31ecac7ae651395e0bd33795cb3e0
mhbughdadi <mohammed_hassan2525@gmail.com>
2026-02-01 16:55:08 +0400

    refactor: update Open AI to use constants instead of string literals

---
+++ src/main/java/com/apogee/product/constants/ProductsConstant.java
@@ -0,0 +1,18 @@
+package com.apogee.product.constants;
+
+public interface ProductsConstant {
+
+    String RECORD_NOT_FOUND = "record.not.found";
+    String PRODUCT_IMAGE_NOT_FOUND = "errors.product.image.not.found";
+    String URL = "url";
+
+    String HTTP_METHOD = "httpMethod";
+    String QUERY_PARAMS = "queryParams";
+    String REQUEST_BODY = "requestBody";
+    String TIMESTAMP = "timestamp";
+    String PATH_VARIABLES = "pathVariables";
+    String HEADERS = "headers";
+    String REQUEST_ID = "requestId";
+    String RESPONSE_BODY = "responseBody";
+    String NULL_STRING = "null";
+}
